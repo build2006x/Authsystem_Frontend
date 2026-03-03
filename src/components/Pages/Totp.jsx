43cd018888beee3d14f9807ecbb98a8f1ed4cc36
barath kumar <mabarathkumar@gmail.com>
2026-03-03 19:19:11 +0530

    mvp of this projec is completed futher enhnancement in future

--- src/components/Pages/Totp.jsx
+++ src/components/Pages/Totp.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Totp = () => {
+  const  [Name,SetName] = useState("")
+  const [imageSrc, setImageSrc] = useState(null);
+  const [password,setpassword] = useState("")
+
+  const TotpRequest = async () =>{
+     const res = await axios.post("http://127.0.0.1:8000/OfflineVerifcation/register",
+      {username:Name},
+      { responseType: "blob" }
+     )
+     // Convert blob into a temporary object URL
+      const imageUrl = URL.createObjectURL(res.data);
+      setImageSrc(imageUrl);
+  } 
+
+  const VerifyPassword = async (e)  =>{
+      e.preventDefault();
+      const res = await axios.get(`http://127.0.0.1:8000/OfflineVerifcation/verify/neels?code=${password}`)
+      console.log(res.data["message"])
+      // if(res.data["message"] == "OTP verified"){
+      //   alert("sucessfully verified") 
+      // } 
+      // else{
+      //   alert("you are no verifed")
+      // }
+  }
+
   return (
     <div className='Totp_auth'>
         <h1>step to get authenticated</h1>
         <br></br>
         <p>1.Install an Authenticator App</p>
-        <p>2.Connect Your Account</p>
+        <p>2.scan the qr shown here</p>
         <p>3.Use the Code to Log In</p>
         <br></br>
-        <div>
+        <div> 
+            <img src={imageSrc} width="150px" ></img>
             <p>scan this qr to get authenticate</p>
+             <input value={Name} onChange={(e)=>SetName(e.target.value)} placeholder='enter the name'/>
+             <br></br>
+             <br></br>
+            <button onClick={TotpRequest}>get the qr code</button>
         </div>
         <br></br>
-        <div className='Verify_division'>
-              <input placeholder='enter the password'/>
+        <form onChange={(e)=> e.preventDefault()}>
+              <input value={password} onChange={(e)=>setpassword(e.target.value)} placeholder='enter the password'/>
               <br></br>
               <br></br>
-              <button>Enter</button>
-        </div>
+              <button onClick={VerifyPassword}> Enter </button>
+        </form>
     </div>
   )
 }
